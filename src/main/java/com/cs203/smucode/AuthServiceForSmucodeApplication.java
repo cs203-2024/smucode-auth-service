2b0a8446f68c5a1f1632819075d525c69c8e9e8b
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-16 20:02:27 +0800

    bootstrap spring initializr

---
+++ src/main/java/com/cs203/smucode/AuthServiceForSmucodeApplication.java
@@ -0,0 +1,13 @@
+package com.cs203.smucode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceForSmucodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceForSmucodeApplication.class, args);
+	}
+
+}
