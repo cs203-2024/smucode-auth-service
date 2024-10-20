f312ad0b0ace17c565f816f5b56ce2db4a79bc20
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-20 14:22:53 +0800

    Add configuration for project component scan

---
+++ src/main/java/com/cs203/smucode/configs/ProjectConfig.java
@@ -0,0 +1,14 @@
+package com.cs203.smucode.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(
+    basePackages = {
+        "com.cs203.smucode.controllers",
+        "com.cs203.smucode.services",
+        "com.cs203.smucode.repositories"
+    }
+)
+public class ProjectConfig {}
