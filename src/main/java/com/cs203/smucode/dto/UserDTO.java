aa7805430fa06a5106117e0173ed5d011c2ffacf
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-20 14:56:03 +0800

    Added message and redefine regex in password

--- src/main/java/com/cs203/smucode/dto/UserDTO.java
+++ src/main/java/com/cs203/smucode/dto/UserDTO.java
@@ -12,7 +12,8 @@ public record UserDTO (
 
         @NotNull(message = "Password cannot be empty/null")
         @Size(min = 8, message = "Password must be more than 8 characters")
-        @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$")
+        @Pattern(message = "Password must contain 1 symbol, 1 uppercase, 1 lowercase and 1 digit",
+                regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).*$")
         String password,
 
         @NotNull(message = "Email cannot be empty/null")
