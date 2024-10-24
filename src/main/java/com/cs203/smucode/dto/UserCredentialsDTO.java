fcd749b232f5269f69ddc77e260085f6f6b5c9a1
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-24 13:14:38 +0800

    Add dtos to model service comm to user svc

--- src/main/java/com/cs203/smucode/dto/UserCredentialsDTO.java
+++ src/main/java/com/cs203/smucode/dto/UserCredentialsDTO.java
@@ -1,6 +1,5 @@
 package com.cs203.smucode.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -8,20 +7,22 @@ import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public record UserCredentialsDTO (
-    @NotNull(message = "Username cannot be empty/null")
+
     UUID id,
 
     @NotNull(message = "Password cannot be empty/null")
     @Size(min = 8, message = "Password must be more than 8 characters")
-    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$")
+    @Pattern(message = "Old Password must contain 1 symbol, 1 uppercase, 1 lowercase and 1 digit",
+            regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$")
     String oldPassword,
 
     @NotNull(message = "Password cannot be empty/null")
     @Size(min = 8, message = "Password must be more than 8 characters")
-    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$")
-    String newPassword,
+    @Pattern(message = "New Password must contain 1 symbol, 1 uppercase, 1 lowercase and 1 digit",
+            regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]*$")
+    String newPassword
 
-    @NotNull(message = "Email cannot be empty/null")
-    @Email(message = "Invalid email format")
-    String email
+//    @NotNull(message = "Email cannot be empty/null")
+//    @Email(message = "Invalid email format")
+//    String email
 ) {}
--- src/main/java/com/cs203/smucode/dto/UserIdentificationDTO.java
+++ src/main/java/com/cs203/smucode/dto/UserIdentificationDTO.java
@@ -1,4 +1,16 @@
 package com.cs203.smucode.dto;
 
-public record UserIdentificationDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserIdentificationDTO(
+        @NotNull(message = "Username cannot be empty/null")
+        String username,
+
+        @NotNull(message = "Email cannot be empty/null")
+        @Email(message = "Invalid email format")
+        String email
+        ) {
 }
