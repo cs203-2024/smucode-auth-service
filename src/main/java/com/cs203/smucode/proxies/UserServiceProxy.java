2cdd5958843219bf0b89000bd35abcd91d0e96a6
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-24 13:19:12 +0800

    Add proxy to talk to user svc

---
+++ src/main/java/com/cs203/smucode/proxies/UserServiceProxy.java
@@ -0,0 +1,36 @@
+package com.cs203.smucode.proxies;
+
+import com.cs203.smucode.dto.UserIdentificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+public class UserServiceProxy {
+    @Value("${user.service.url}")
+    private String userUrl;
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public UserServiceProxy(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public void createUserProfile(UUID id, String username, String email) {
+        String createProfileUrl = userUrl + "/create";
+        UserIdentificationDTO dto = new UserIdentificationDTO(id, username, email);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(createProfileUrl, dto, String.class);
+
+        if (response.getStatusCode() != HttpStatus.CREATED) {
+            throw new RuntimeException("Error creating user profile: " + dto);
+        }
+    }
+
+}
