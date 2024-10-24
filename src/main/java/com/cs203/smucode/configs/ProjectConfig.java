98579e8916535e15e9ab99e6efc881f5cdf22258
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-24 13:14:20 +0800

    Update config to include RestTemplate

--- src/main/java/com/cs203/smucode/configs/ProjectConfig.java
+++ src/main/java/com/cs203/smucode/configs/ProjectConfig.java
@@ -1,14 +1,23 @@
 package com.cs203.smucode.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @ComponentScan(
     basePackages = {
         "com.cs203.smucode.controllers",
         "com.cs203.smucode.services",
-        "com.cs203.smucode.repositories"
+        "com.cs203.smucode.repositories",
+        "com.cs203.smucode.proxies"
     }
 )
-public class ProjectConfig {}
+public class ProjectConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
