88e3b6b0fb235d3525a207eecb2f06755a8a8ebc
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-24 13:14:59 +0800

    Update controller for user profile gen on signup

--- src/main/java/com/cs203/smucode/controllers/AuthController.java
+++ src/main/java/com/cs203/smucode/controllers/AuthController.java
@@ -7,10 +7,13 @@ import com.cs203.smucode.exception.ApiRequestException;
 import com.cs203.smucode.mappers.UserMapper;
 import com.cs203.smucode.models.User;
 import com.cs203.smucode.dto.UserDTO;
+import com.cs203.smucode.proxies.UserServiceProxy;
 import com.cs203.smucode.services.IUserService;
 import com.cs203.smucode.utils.JWTUtil;
 import jakarta.validation.Valid;
 import jakarta.validation.ValidationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -33,15 +36,21 @@ import org.springframework.http.ResponseEntity;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     private final IUserService userService;
     private final JWTUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
+    private final UserServiceProxy userServiceProxy;
 
     @Autowired
-    public AuthController(IUserService userService, JWTUtil jwtUtil, AuthenticationManager authenticationManager) {
+    public AuthController(IUserService userService, JWTUtil jwtUtil,
+                          AuthenticationManager authenticationManager,
+                          UserServiceProxy userServiceProxy) {
         this.userService = userService;
         this.jwtUtil = jwtUtil;
         this.authenticationManager = authenticationManager;
+        this.userServiceProxy = userServiceProxy;
     }
 
     @PostMapping("/login")
@@ -87,6 +96,7 @@ public class AuthController {
 
     @PostMapping("/signup")
     public ResponseEntity<UserDTO> signup(@RequestBody @Valid UserDTO dto) {
+        // Validate request
         if (dto.username() == null || dto.username().isEmpty()) {
             throw new ApiRequestException("Username cannot be null or empty");
         }
@@ -96,17 +106,21 @@ public class AuthController {
         }
 
         try {
+            // Save the user with their authorities
             User createdUser = UserMapper.INSTANCE.userDTOtoUser(dto);
             userService.createUser(createdUser);
+
+            // Talk to user service to create a profile for this user
+            userServiceProxy.createUserProfile(dto.username(), dto.email());
 
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(UserMapper.INSTANCE.userToUserDTO(createdUser));
         } catch (DataIntegrityViolationException e) {
             throw new ApiRequestException("That username/email already exists, please try another", e);
         } catch (ValidationException e) {
-            throw new ApiRequestException(e.getMessage(), e);
+            throw new ApiRequestException(e.getMessage());
         } catch (Exception e) {
-            throw new ApiRequestException("An error occurred during signup", e);
+            throw new ApiRequestException("An error occurred during signup");
         }
     }
 
@@ -116,7 +130,7 @@ public class AuthController {
         return ResponseEntity.ok("User logged out successfully");
     }
 
-    @PostMapping("/delete-account")
+    @DeleteMapping("/delete-account")
     public ResponseEntity<String> deleteAccount(@RequestBody @Valid UserCredentialsDTO dto) {
         if (dto.id() == null) {
             throw new ApiRequestException("UUID cannot be null or empty");
@@ -126,12 +140,13 @@ public class AuthController {
             userService.deleteUser(dto.id(), dto.oldPassword());
             return ResponseEntity.ok("User deleted successfully");
         } catch (Exception e) {
-            throw new ApiRequestException("An error occurred during delete account", e);
+            throw new ApiRequestException("An error occurred during delete account");
         }
     }
 
-    @PostMapping("/change-password")
+    @PutMapping("/change-password")
     public ResponseEntity<String> resetPassword(@RequestBody @Valid UserCredentialsDTO dto) {
+        logger.info("change-password received" + dto.toString());
         if (dto.id() == null) {
             throw new ApiRequestException("UUID cannot be null or empty");
         }
@@ -148,8 +163,7 @@ public class AuthController {
             userService.updatePassword(dto.id(), dto.oldPassword(), dto.newPassword());
             return ResponseEntity.ok("Password changed successfully");
         } catch (Exception e) {
-            throw new ApiRequestException("An error occurred during password reset", e);
+            throw new ApiRequestException("An error occurred during password reset");
         }
     }
-
 }
