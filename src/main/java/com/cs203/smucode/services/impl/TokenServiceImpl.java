71727497d26f55509018d74dbb6aa11677a22c34
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-26 22:32:24 +0800

    Add definition of token service

--- src/main/java/com/cs203/smucode/services/impl/TokenServiceImpl.java
+++ src/main/java/com/cs203/smucode/services/impl/TokenServiceImpl.java
@@ -4,10 +4,12 @@ import com.cs203.smucode.models.RefreshToken;
 import com.cs203.smucode.models.User;
 import com.cs203.smucode.repositories.RefreshTokenRepository;
 import com.cs203.smucode.repositories.UserRepository;
-import com.cs203.smucode.services.IRefreshTokenService;
+import com.cs203.smucode.services.ITokenService;
+import com.cs203.smucode.utils.JWTUtil;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -16,45 +18,47 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Service
-public class RefreshTokenServiceImpl implements IRefreshTokenService {
+public class TokenServiceImpl implements ITokenService {
 
     @Value("${jwt.refresh.duration}")
-    private Duration REFRESH_TOKEN_VALIDITY = Duration.ofMinutes(120);
+    private long REFRESH_TOKEN_VALIDITY;
 
     private final RefreshTokenRepository refreshTokenRepository;
     private final UserRepository userRepository;
+    private final JWTUtil jwtUtil;
 
     @Autowired
-    public RefreshTokenService(RefreshTokenRepository refreshTokenRepository,
-                               UserRepository userRepository) {
+    public TokenServiceImpl(RefreshTokenRepository refreshTokenRepository,
+                               UserRepository userRepository, JWTUtil jwtUtil) {
         this.refreshTokenRepository = refreshTokenRepository;
         this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
     @Transactional
     // username as if we redirect request, uuid may not be in every request?
-    public RefreshToken createRefreshToken(String username) {
+    public String createRefreshToken(String username) {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
 
         final UUID tokenId = UUID.randomUUID();
         final RefreshToken refreshToken = new RefreshToken();
 
-        refreshToken.setId(tokenId);
+        refreshToken.setToken(tokenId);
         refreshToken.setUser(user);
-        refreshToken.setExpiresAt(OffsetDateTime.now().plus(REFRESH_TOKEN_VALIDITY));
+        refreshToken.setExpiresAt(OffsetDateTime.now().plus(Duration.ofMinutes(REFRESH_TOKEN_VALIDITY)));
 
         refreshTokenRepository.save(refreshToken);
-        return refreshToken;
+        return refreshToken.getToken().toString();
     }
 
     @Override
     @Transactional
-    public boolean validateRefreshToken(UUID refreshToken) {
+    public RefreshToken validateRefreshToken(UUID refreshToken) {
         return refreshTokenRepository
                 .findByTokenAndExpiresAtAfter(refreshToken, OffsetDateTime.now())
-                .isPresent();
+                .orElse(null);
     }
 
     @Override
@@ -67,4 +71,17 @@ public class RefreshTokenServiceImpl implements IRefreshTokenService {
                     refreshTokenRepository.save(invalidToken);
                 });
     }
+
+    @Override
+    @Transactional
+    public String createAccessToken(UserDetails userDetails) {
+        return jwtUtil.generateToken(userDetails);
+    }
+
+
+    @Override
+    public JWTUtil getJWTUtil() {
+        return this.jwtUtil;
+    }
+
 }
