8eb288835096296e628fa63507b1b78d3846268e
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-29 12:16:06 +0800

    refactor: change validation message for login request

--- src/main/java/com/cs203/smucode/dto/LoginRequestDTO.java
+++ src/main/java/com/cs203/smucode/dto/LoginRequestDTO.java
@@ -1,17 +1,11 @@
 package com.cs203.smucode.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-
 public record LoginRequestDTO(
-        @NotNull(message = "Username cannot be empty/null")
+        @NotNull(message = "Invalid login credentials")
         String username,
 
-        @NotNull(message = "Password cannot be empty/null")
-        @Size(min = 8, message = "Password must be more than 8 characters")
-        @Pattern(message = "Password must contain 1 symbol, 1 uppercase, 1 lowercase and 1 digit",
-                regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).*$")
+        @NotNull(message = "Invalid login credentials")
         String password
 ) {
 }
