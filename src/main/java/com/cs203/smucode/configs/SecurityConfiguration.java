e3ecf93b58a86d0451ed648db0cae6b086053281
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-29 22:28:03 +0800

    fix: add user to permitted routes

--- src/main/java/com/cs203/smucode/configs/SecurityConfiguration.java
+++ src/main/java/com/cs203/smucode/configs/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
                         "/api/auth/logout",
                         "/api/auth/change-password",
                         "/api/auth/delete-account")
-                .hasAuthority("SCOPE_ROLE_ADMIN")
+                .hasAnyAuthority("SCOPE_ROLE_ADMIN", "SCOPE_ROLE_USER")
                 .anyRequest()
                 .permitAll()
         );
