7f6d83fd94dc938f6889082a856f472e5d605fca
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2024-11-10 15:11:32 +0800

    Fix smell

--- src/test/java/com/cs203/smucode/services/impl/AuthUserServiceImplTest.java
+++ src/test/java/com/cs203/smucode/services/impl/AuthUserServiceImplTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class AuthUserServiceImplTest {
+class AuthUserServiceImplTest {
     @Mock
     private IUserService userService;
 
