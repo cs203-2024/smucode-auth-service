bf196c28ac456a24b4b57c73b080e9d2c9607d29
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-11-11 14:32:16 +0800

    refactor: allow feign to discover clients in eureka

--- src/main/java/com/cs203/smucode/proxies/UserServiceProxy.java
+++ src/main/java/com/cs203/smucode/proxies/UserServiceProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@FeignClient(name = "user-service", url="${user.service.url}")
+@FeignClient(name = "user-service")
 public interface UserServiceProxy {
     @PostMapping("/profile/create")
     void createUserProfile(@RequestBody @Valid UserIdentificationDTO userIdentificationDTO);
