39919b18b628531d11d6d62a0252bdb2c74104b8
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-11-11 15:30:38 +0800

    fix: move back to old build

--- src/main/java/com/cs203/smucode/proxies/UserServiceProxy.java
+++ src/main/java/com/cs203/smucode/proxies/UserServiceProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url="${user.service.url}")
 public interface UserServiceProxy {
     @PostMapping("/profile/create")
     void createUserProfile(@RequestBody @Valid UserIdentificationDTO userIdentificationDTO);
